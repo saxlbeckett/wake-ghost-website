7343a29b8895d24bbc44b21a391af422a8e2530f
Scott <saxlbeckett@gmail.com>
2021-04-19 11:24:45 -0400

    Started building a data structure.

--- client/src/components/content/FrontPage.js
+++ client/src/components/content/FrontPage.js
@@ -3,7 +3,25 @@ import { Link } from 'react-router-dom'
 
 
 const FrontPage = () => {
+  const wakeGhostObject = {
+    albums: [
+      {
+        title: "What is the Reason for Life?",
+        tracks: [
+          {
+            one: "Ghost Warship",
+            audioFile: "https://audiofilestorage2.s3.amazonaws.com/Ghost+Warship.mp3",
+            lyrics: "",
+            image: "https://audiofilestorage2.s3.amazonaws.com/Wake+Ghost+Cover_Demo+Fest.jpeg"
+          },
+          {
+            two: "Not Now Madeline"
+          }
+        ],
 
+      }
+    ]
+  }
   return(
     <div>
       <h1>Wake Ghost</h1>
